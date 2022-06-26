1cc5a1fd917c03a89ce3757a2de7a62f87ea4513
rom2110 <92000053+rom2110@users.noreply.github.com>
2022-06-26 04:54:09 -0400

    added try for server socket

--- src/Slave.java
+++ src/Slave.java
@@ -6,37 +6,38 @@ public class Slave{
     final static int PORT = 12300;
     public static void main(String[] args) throws Exception {
 
-        ServerSocket servsock = new ServerSocket(PORT);
-        //create server socket
-        while(true){
-            
-            Socket sock = servsock.accept();
-
-            //create Object Input Stream
-            ObjectInputStream objIn = new ObjectInputStream(sock.getInputStream());
-
-            Robot robot = new Robot();
-
-            while(!sock.isClosed()){
-                try{
-                    boolean pressed = (boolean) objIn.readObject();
-                    if(pressed){
-                        int keyRead = (int) objIn.readObject();
-                        System.out.println("Key Pressed: " + keyRead);
-                        robot.keyPress(keyRead);
-                    } 
-                    
-                    else {
-                        int keyRead = (int) objIn.readObject();
-                        System.out.println("Key Released: " + keyRead);
-                        robot.keyRelease(keyRead);
-                    } 
-                } catch(Exception e){
-                    //cleanup
-                    objIn.close();
-                    sock.close();
-                }
+        try (ServerSocket servsock = new ServerSocket(PORT)) {
+            //create server socket
+            while(true){
                 
+                Socket sock = servsock.accept();
+
+                //create Object Input Stream
+                ObjectInputStream objIn = new ObjectInputStream(sock.getInputStream());
+
+                Robot robot = new Robot();
+
+                while(!sock.isClosed()){
+                    try{
+                        boolean pressed = (boolean) objIn.readObject();
+                        if(pressed){
+                            int keyRead = (int) objIn.readObject();
+                            System.out.println("Key Pressed: " + keyRead);
+                            robot.keyPress(keyRead);
+                        } 
+                        
+                        else {
+                            int keyRead = (int) objIn.readObject();
+                            System.out.println("Key Released: " + keyRead);
+                            robot.keyRelease(keyRead);
+                        } 
+                    } catch(Exception e){
+                        //cleanup
+                        objIn.close();
+                        sock.close();
+                    }
+                    
+                }
             }
         }
     }
