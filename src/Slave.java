1d2453fb9ee08e3b18e7a7d046a580c8adf7f221
Richard Mouradian <richardmouradian@Richards-MacBook-Air.local>
2022-06-26 04:52:39 -0400

    modified slave so that it can be reconnected if master disconnects

--- src/Slave.java
+++ src/Slave.java
@@ -6,33 +6,38 @@ public class Slave{
     final static int PORT = 12300;
     public static void main(String[] args) throws Exception {
 
-        //create server socket
         ServerSocket servsock = new ServerSocket(PORT);
-        Socket sock = servsock.accept();
+        //create server socket
+        while(true){
+            
+            Socket sock = servsock.accept();
 
-        //create Object Input Stream
-        ObjectInputStream objIn = new ObjectInputStream(sock.getInputStream());
+            //create Object Input Stream
+            ObjectInputStream objIn = new ObjectInputStream(sock.getInputStream());
 
-        Robot robot = new Robot();
+            Robot robot = new Robot();
 
-        while(!sock.isClosed()){
-            boolean pressed = (boolean) objIn.readObject();
-            if(pressed){
-                    int keyRead = (int) objIn.readObject();
-                    System.out.println("Ket Pressed: " + keyRead);
-                    robot.keyPress(keyRead);
-                } 
+            while(!sock.isClosed()){
+                try{
+                    boolean pressed = (boolean) objIn.readObject();
+                    if(pressed){
+                        int keyRead = (int) objIn.readObject();
+                        System.out.println("Key Pressed: " + keyRead);
+                        robot.keyPress(keyRead);
+                    } 
+                    
+                    else {
+                        int keyRead = (int) objIn.readObject();
+                        System.out.println("Key Released: " + keyRead);
+                        robot.keyRelease(keyRead);
+                    } 
+                } catch(Exception e){
+                    //cleanup
+                    objIn.close();
+                    sock.close();
+                }
                 
-            else {
-                    int keyRead = (int) objIn.readObject();
-                    System.out.println("Ket Released: " + keyRead);
-                    robot.keyRelease(keyRead);
-                } 
+            }
         }
-
-        //cleanup
-        sock.close();
-        servsock.close();
-
     }
 }
